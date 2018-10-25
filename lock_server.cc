7d93302a2a80eb7f5c9666286ba16813157b9afc
Vlad Skripniuk <skripniuk.v@yandex.ru>
2018-10-25 17:52:31 +0200

    naive release and acquire

--- lock_server.cc
+++ lock_server.cc
@@ -20,4 +20,39 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   return ret;
 }
 
+lock_protocol::status
+lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
+{
+	printf("acquire request from clt %d\n", clt);
+
+	std::map<lock_protocol::lockid_t,int>::iterator it;
+	it = locks.find(lid);
+
+	if ((it->second == FREE) || (it == locks.end())) {
+		nacquire = nacquire + 1;
+		locks[lid] = LOCKED;
+	}
+
+  r = nacquire;
+  lock_protocol::status ret = lock_protocol::OK;
+  return ret;
+}
+
+lock_protocol::status
+lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
+{
+	printf("release request from clt %d\n", clt);
+
+	std::map<lock_protocol::lockid_t,int>::iterator it;
+	it = locks.find(lid);
+	
+	if ((it != locks.end()) && (it->second == LOCKED)) {
+		nacquire = nacquire - 1;
+		locks[lid] = FREE;
+	}
+
+  r = nacquire;
+  lock_protocol::status ret = lock_protocol::OK;
+  return ret;
+}
 
--- lock_server.h
+++ lock_server.h
@@ -5,6 +5,7 @@
 #define lock_server_h
 
 #include <string>
+#include <map>
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
@@ -13,11 +14,17 @@ class lock_server {
 
  protected:
   int nacquire;
+  std::map<lock_protocol::lockid_t, int> locks;
 
  public:
+ 	static const int FREE = 0;
+ 	static const int LOCKED = 1;
+
   lock_server();
   ~lock_server() {};
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
 };
 
 #endif 
