ff9ab9a92ac4f1f8ca339a2ea9f7e9bfce73b1a2
Sergey Bozhko <sbozhko@mpi-sws.org>
2018-11-07 14:44:35 +0100

    Update

--- fuse.cc
+++ fuse.cc
@@ -125,8 +125,9 @@ yfs_client::status
 fuseserver_createhelper(fuse_ino_t parent, const char *name, mode_t mode, struct fuse_entry_param *e)
 {
   std::cout << "void fuseserver_createhelper\n";
+  
   // You fill this in
-  return yfs_client::NOENT;
+  return yfs->create(parent, name);
 }
 
 void
@@ -134,7 +135,7 @@ fuseserver_create(fuse_req_t req, fuse_ino_t parent, const char *name, mode_t mo
 {
   std::cout << "void fuseserver_create\n";
   struct fuse_entry_param e;
-  if( fuseserver_createhelper( parent, name, mode, &e ) == yfs_client::OK ) {
+  if(fuseserver_createhelper( parent, name, mode, &e ) == yfs_client::OK ) {
     fuse_reply_create(req, &e, fi);
   } else {
     fuse_reply_err(req, ENOENT);
