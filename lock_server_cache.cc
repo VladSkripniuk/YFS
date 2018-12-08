7d902465b82dc2a13e114a6277e2177673a50d4f
Vlad Skripniuk <skripniuk.v@yandex.ru>
2018-12-08 18:47:27 +0100

    sketch of lock server

--- lock_server_cache.cc
+++ lock_server_cache.cc
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <cstdio>
 
 static void *
 revokethread(void *x)
@@ -24,6 +25,9 @@ retrythread(void *x)
 
 lock_server_cache::lock_server_cache()
 {
+  pthread_mutex_init(&locks_map_mutex, NULL);
+  pthread_mutex_init(&lock_clients_map_mutex, NULL);
+ 
   pthread_t th;
   int r = pthread_create(&th, NULL, &revokethread, (void *) this);
   assert (r == 0);
@@ -38,6 +42,12 @@ lock_server_cache::revoker()
   // This method should be a continuous loop, that sends revoke
   // messages to lock holders whenever another client wants the
   // same lock
+  lock_protocol::lockid_t lid;
+  while (1) {
+    lid = revoker_queue.pop_front();
+
+    // TODO: send revoke RPC
+  }
 
 }
 
@@ -49,8 +59,92 @@ lock_server_cache::retryer()
   // This method should be a continuous loop, waiting for locks
   // to be released and then sending retry messages to those who
   // are waiting for it.
+  lock_protocol::lockid_t lid;
+  while (1) {
+    lid = retrier_queue.pop_front();
+
+    // TODO: send retry RPC
+  }
 
 }
 
 
+lock_protocol::status
+lock_server_cache::stat(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  r = nacquire;
+  return lock_protocol::OK;
+}
 
+
+lock_protocol::status
+lock_server_cache::acquire(int clt, std::string client_socket, lock_protocol::seqnum_t seqnum, lock_protocol::lockid_t lid, int &r) {
+  
+  std::cout << "acquire request (clt " << client_socket << ", seqnum " << seqnum << ", lock id: " << lid << ")\n";
+  pthread_mutex_lock(&release_acquire_mutex);
+
+  std::map<lock_protocol::lockid_t, lock>::iterator it;
+  it = locks.find(lid);
+
+  if (it == locks.end()) {
+    lock new_lock;
+    locks[lid] = new_lock;
+    it = locks.find(lid);
+  }
+
+  if (it->second.is_free()) {
+    std::map<std::string, lock_client_info>::iterator it;
+    it = lock_clients.find(client_socket);
+
+    if (it == lock_clients.end()) {
+      lock_client_info new_lock_client_info(client_socket);
+
+      lock_clients[client_socket] = new_lock_client_info;
+      it = lock_clients.find(client_socket);
+    }
+
+    it->second.nacquire += 1;
+    locks[lid].grant_lock(client_socket, lid);
+    
+  }
+  else {
+    locks[lid].add_to_waiting_list(client_socket, lid);
+
+    revoker_queue.push_back(lid);
+
+  }
+  
+  std::cout << "acquire done (clt " << clt << ", lock id: " << lid << ")\n";
+  pthread_mutex_unlock(&release_acquire_mutex);
+
+  return lock_protocol::OK;
+}
+
+
+lock_protocol::status
+lock_server_cache::release(int clt, std::string client_socket, lock_protocol::seqnum_t seqnum, lock_protocol::lockid_t lid, int &r) {
+  
+  std::cout << "release request (clt " << clt << ", lock id: " << lid << ")\n";
+  pthread_mutex_lock(&release_acquire_mutex);
+
+  std::map<lock_protocol::lockid_t,lock>::iterator it;
+  it = locks.find(lid);
+
+  if ((it != locks.end()) && (it->second.lock_state == LOCKED)) {
+    nacquire--;
+
+    std::map<std::string,lock_client_info>::iterator it;
+    it = lock_clients.find(client_socket);
+    if (it != lock_clients.end()) {
+      it->nacquire -= 1;
+    }
+
+    it->second.lock_state = FREE;
+    
+    retrier_queue.push_back(lid);
+  }
+ 
+  pthread_mutex_unlock(&release_acquire_mutex);
+  
+  return lock_protocol::OK;
+}
--- lock_server_cache.h
+++ lock_server_cache.h
@@ -7,13 +7,125 @@
 #include "lock_server.h"
 
 
+struct lock_client_id_and_seqnum {
+	std::string id;
+	lock_protocol::seqnum_t seqnum;
+};
+
+template<class T>
+class thread_safe_queue {
+ // method invokation blocks on a mutex, though I think
+ // it's ok to use this queue in handlers, which should
+ // run to completion without blocking, since mutex is
+ // locked for very short time, and no RPCs are called while holding this mutex
+private:
+	std::list<T> queue;
+	pthread_mutex_t m;
+	pthread_cond_t cv;
+public:
+	thread_safe_queue() {
+		pthread_mutex_init(&m, NULL);
+		pthread_cond_init(&cv, NULL);
+	}
+	void push_back(T a) {
+		pthread_mutex_lock(&m);
+		queue.push_back(a);
+		pthread_cond_broadcast(&cv);
+		pthread_mutex_unlock(&m);
+	}
+	T pop_front() {
+		pthread_mutex_lock(&m);
+		T r;
+		while(1) {
+	    if (!queue.empty()) {
+	      r = queue.pop_front()
+	      break;
+	    }
+	    pthread_cond_wait(&cv, &m);
+	  }
+		pthread_mutex_unlock(&m);
+		return r;
+	}
+};
+
 
 class lock_server_cache {
  public:
   lock_server_cache();
-  lock_protocol::status stat(lock_protocol::lockid_t, int &);
   void revoker();
   void retryer();
+  lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, std::string client_socket, lock_protocol::seqnum_t seqnum, lock_protocol::lockid_t lid, int &r);
+  // lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
+
+protected:
+  int nacquire = 0;
+
+  class lock {
+  private:
+    static const int FREE = 0;
+  	static const int LOCKED = 1;
+
+    int lock_state;
+    // pthread_cond_t cond_var;
+    std::string owner; // lock_client_cache::id
+    lock_protocol::seqnum_t seqnum; // seqnum of last acquire
+    std::list<lock_client_id_and_seqnum> waiting_list;
+
+  public:
+    lock() {
+      lock_state = FREE;
+      // pthread_cond_init(&cond_var, NULL);
+    }
+
+    bool is_free() { return (lock_state == FREE); }
+
+    void grant_lock(std::string client_id, lock_protocol::seqnum_t seqnum_of_request) {
+    	lock_state = LOCKED;
+    	owner = client_id;
+    	seqnum = seqnum_of_request;
+    }
+
+    void release_lock() {
+    	lock_state = FREE;
+    }
+
+    void add_to_waiting_list(std::string client_id, lock_protocol::seqnum_t seqnum_of_request) {
+    	lock_client_id_and_seqnum t;
+    	t.id = client_id;
+    	t.seqnum = seqnum_of_request;
+    	waiting_list.push_back(t);
+    }
+  };
+
+  struct lock_client_info {
+  	int nacquire;
+  	rpcc *cl;
+
+  	lock_client_info(std::string client_socket) {
+  		nacquire = 0;
+
+  		sockaddr_in dstsock;
+      make_sockaddr(client_socket.c_str(), &dstsock);
+      cl = new rpcc(dstsock);
+      if (cl->bind() < 0) {
+        printf("lock_client: call bind\n");
+      }
+  	}
+
+
+  };
+    
+protected:
+  int nacquire;
+  std::map<lock_protocol::lockid_t, lock> locks;
+  std::map<std::string, lock_client_info> lock_clients;
+  pthread_mutex_t release_acquire_mutex; // this mutex protects locks, lock_clients and most importantly, nacquire
+
+  thread_safe_queue<lock_protocol::lockid_t> retrier_queue; // push_back to safe_queue wakes up retrier
+  thread_safe_queue<lock_protocol::lockid_t> revoker_queue; // push_back to safe_queue wakes up revoker
+
 };
 
 #endif
