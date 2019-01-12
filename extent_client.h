7b23ddf807ef435a972b9835a3107f9f9d75524a
Sergey Bozhko <gkerfimf@gmail.com>
2019-01-12 19:04:32 +0100

    Add better tabulation in extent_client

--- extent_client.h
+++ extent_client.h
@@ -34,14 +34,11 @@ class extent_client {
   pthread_mutex_t buffers_mutex;
   std::map<extent_protocol::extentid_t, attr_buf_cached> buffers;
 
-
  public:
   extent_client(std::string dst);
 
-  extent_protocol::status get(extent_protocol::extentid_t eid, 
-			      std::string &buf);
-  extent_protocol::status getattr(extent_protocol::extentid_t eid, 
-				  extent_protocol::attr &a);
+  extent_protocol::status get(extent_protocol::extentid_t eid, std::string &buf);
+  extent_protocol::status getattr(extent_protocol::extentid_t eid, extent_protocol::attr &a);
   extent_protocol::status put(extent_protocol::extentid_t eid, std::string buf);
   extent_protocol::status remove(extent_protocol::extentid_t eid);
   extent_protocol::status flush(extent_protocol::extentid_t eid);
