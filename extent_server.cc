d1d3b34a5a59e3ab5eb7a26eeffb74d6e2939b34
Sergey Bozhko <gkerfimf@gmail.com>
2019-01-12 19:06:51 +0100

    Add better tabulation in extent_server

--- extent_server.cc
+++ extent_server.cc
@@ -11,34 +11,34 @@
 #include <ctime>
 
 extent_server::extent_server() {
-  int t;
-  this->put(1, std::string(""), t);
+    int t;
+    this->put(1, std::string(""), t);
 }
 
 
 int extent_server::put(extent_protocol::extentid_t id, std::string buf, int &)
 {
-  ScopedLock scoped_lock(&buffers_mutex);
-  std::cout << "put " << "<" <<id << "><" << buf << ">" << std::endl;
-  auto it = buffers.find(id);
+    ScopedLock scoped_lock(&buffers_mutex);
+    std::cout << "put " << "<" <<id << "><" << buf << ">" << std::endl;
+    auto it = buffers.find(id);
     
     // Add empty buf
-  if (it == buffers.end()) {
-    attr_buf new_buffer;
-    buffers[id] = new_buffer;
-    it = buffers.find(id);
-  }
-
+    if (it == buffers.end()) {
+        attr_buf new_buffer;
+        buffers[id] = new_buffer;
+        it = buffers.find(id);
+    }
+    
     // Fill in
-  it->second.buf = buf;
-  it->second.attr.size = buf.length();
-
-  time_t t = time(NULL);
-
-  it->second.attr.ctime = t;
-  it->second.attr.mtime = t;
-
-  return extent_protocol::OK;
+    it->second.buf = buf;
+    it->second.attr.size = buf.length();
+    
+    time_t t = time(NULL);
+    
+    it->second.attr.ctime = t;
+    it->second.attr.mtime = t;
+    
+    return extent_protocol::OK;
 }
 
 int extent_server::get(extent_protocol::extentid_t id, std::string &buf)
@@ -61,28 +61,28 @@ int extent_server::get(extent_protocol::extentid_t id, std::string &buf)
 
 int extent_server::getattr(extent_protocol::extentid_t id, extent_protocol::attr &a)
 {
-  ScopedLock scoped_lock(&buffers_mutex);
-  auto it = buffers.find(id);
-  if (it == buffers.end()) {
-    return extent_protocol::NOENT;
-  }
-
-  a = it->second.attr;
-  return extent_protocol::OK;
+    ScopedLock scoped_lock(&buffers_mutex);
+    auto it = buffers.find(id);
+    if (it == buffers.end()) {
+        return extent_protocol::NOENT;
+    }
+    
+    a = it->second.attr;
+    return extent_protocol::OK;
 }
 
 int extent_server::remove(extent_protocol::extentid_t id, int &)
 {
-  ScopedLock scoped_lock(&buffers_mutex);
-  auto it = buffers.find(id);
-  if (it == buffers.end()) {
-    return extent_protocol::NOENT;
-  }
-
-  buffers.erase(it);
-
-  return extent_protocol::OK;
-
-  // return extent_protocol::IOERR;
+    ScopedLock scoped_lock(&buffers_mutex);
+    auto it = buffers.find(id);
+    if (it == buffers.end()) {
+        return extent_protocol::NOENT;
+    }
+    
+    buffers.erase(it);
+    
+    return extent_protocol::OK;
+    
+    // return extent_protocol::IOERR;
 }
 
